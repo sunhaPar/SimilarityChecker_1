c4216bca6c18554f67161f348f28050e932aab6e
sunha <sunha0710@naver.com>
2024-05-31 09:17:45 +0900

    length checker 구현

--- Project14/similarityChecker.cpp
+++ Project14/similarityChecker.cpp
@@ -1,4 +1,46 @@
+#include <string>
+
+using namespace std;
+
+const int MaxLengthScore = 60;
+const int MaxAlphaScore = 40;
+
 class SimilarityChecker {
 public:
+	explicit SimilarityChecker(const string& target)
+		:target(target)
+	{
+
+	}
+
+	int checkLength(const string& guess)
+	{
+		if (guess.size() == target.size()) return MaxLengthScore;
+
+		return getLenghScore(guess);
+	}
+
+private:
+	int getLenghScore(const string& guess)
+	{
+		int small = 0;
+		int gap = 0;
+		if (guess.size() > target.size())
+		{
+			gap = guess.size() - target.size();
+			small = target.size();
+		}
+		else
+		{
+			gap = target.size() - guess.size();
+			small = guess.size();
+		}
+
+		if (gap >= small)   return 0;  //double Gap
+
+		return (MaxLengthScore * (small - gap) / small);
+	}
+
+	string target;
 
 };
--- Sample-Test1/similarityChecker_test.cpp
+++ Sample-Test1/similarityChecker_test.cpp
@@ -3,7 +3,31 @@
 #include <string>
 #include "../Project14/similarityChecker.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+class CheckerFixture : public testing::Test {
+public:
+	SimilarityChecker checker{ "ABCDE" };
+};
+
+
+TEST_F(CheckerFixture, LengthTestSameLength) {
+	string guess = "DEFGY";
+	
+	int ret = checker.checkLength(guess);
+	EXPECT_EQ(ret, 60);
+}
+
+TEST_F(CheckerFixture, LengthTestDoubleGap) {
+	string guess = "B";
+
+	int ret = checker.checkLength(guess);
+	EXPECT_EQ(ret, 0);
+}
+
+TEST_F(CheckerFixture, LengthTestPartial) {
+	string guess = "ABC";
+
+	int ret = checker.checkLength(guess);
+	EXPECT_EQ(ret, 20);
 }
